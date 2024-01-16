ce406d2f4a318a46eaf4895fe8b6ed80b43a3bc3
boris <yuhaoboris@gmail.com>
2024-01-16 16:10:39 +0800

    add: take photo and preview

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,71 @@
+import { Alert, Button, Image, View, Text, StyleSheet } from 'react-native'
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker'
+import { useState } from 'react'
+
+import { Colors } from '../../constants/colors'
+
+function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] = useCameraPermissions()
+  const [pickedImage, setPickedImage] = useState()
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission()
+      return permissionResponse.granted
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert('Insufficient Permissions', 'You need to grant camera perssion to use this app.')
+      return false
+    }
+
+    return true
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions()
+
+    if (!hasPermission) {
+      return
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    })
+
+    setPickedImage(image.assets[0].uri)
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>
+
+  if (pickedImage) {
+    imagePreview = <Image source={{ uri: pickedImage }} style={styles.image} />
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary700,
+    borderRadius: 4,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+})
+
+export default ImagePicker
